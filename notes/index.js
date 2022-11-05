ca22d411c178b58c31cc85c9daa08936a4d7b62b
Camila <camilagbortolotti@gmail.com>
2022-11-05 08:40:55 -0300

    Notes get request

--- notes/index.js
+++ notes/index.js
@@ -4,9 +4,14 @@ const app = express();
 const notes = {};
 
 app.get ('/notes', (req, res) => {
+    res.send(notes);
 
 });
 
 app.post('/notes', (req, res) => {
 
 });
+
+app.listen(4000, () => {
+    console.log('Notes. Port 4000');
+})
